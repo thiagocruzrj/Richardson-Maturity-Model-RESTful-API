959212cb77e83b7115ae255f1b8d324d24e48d9d
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-11-22 14:12:10 -0300

    Added DataContract

--- LibraryApi/Data/VO/BookVO.cs
+++ LibraryApi/Data/VO/BookVO.cs
@@ -1,13 +1,20 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace LibraryApi.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        [DataMember (Order = 1)]
         public long? Id { get; set; }
+        [DataMember (Order = 2, Name ="titulo")]
         public string Title { get; set; }
+        [DataMember (Order = 3, Name ="autor")]
         public string Author { get; set; }
+        [DataMember (Order = 5, Name ="preco")]
         public decimal Price { get; set; }
+        [DataMember (Order = 4, Name ="dataLancamento")]
         public DateTime LaunchDate { get; set; }
     }
 }
